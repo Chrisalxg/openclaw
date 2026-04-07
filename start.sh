f512104510daa51ed48ce0554a6dc4f67a5d3b0a
Chrisalxg <chrisalxg@gmail.com>
2026-04-07 09:29:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 openclaw config set gateway.bind lan || true
 openclaw config set gateway.controlUi.dangerouslyAllowHostHeaderOriginFallback true || true
+openclaw config set gateway.auth.token nikhil-law-2026 || true
 exec openclaw gateway --allow-unconfigured
