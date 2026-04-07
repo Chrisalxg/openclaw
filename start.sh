e99575391742ef6de3ba050355095fec80666bac
Chrisalxg <chrisalxg@gmail.com>
2026-04-07 09:15:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
-node /app/openclaw.mjs config set gateway.bind lan || true
-node /app/openclaw.mjs config set gateway.controlUi.dangerouslyAllowHostHeaderOriginFallback true || true
-exec node /app/openclaw.mjs gateway --allow-unconfigured
+openclaw config set gateway.bind lan || true
+openclaw config set gateway.controlUi.dangerouslyAllowHostHeaderOriginFallback true || true
+exec openclaw gateway --allow-unconfigured
